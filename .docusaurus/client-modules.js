ddd2c62eca6127ed9d61a361311c67258391f740
Peter Graham <petergrahamjersey@gmail.com>
2024-06-01 17:28:00 +0100

    Updating package dependencies

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/petergraham/Documents/Projects/peter-graham-site/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/petergraham/Documents/Projects/peter-graham-site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/petergraham/Documents/Projects/peter-graham-site/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/petergraham/Documents/Projects/peter-graham-site/src/css/custom.css'),
-];
